ad517f7bc199e8d87673f8a58e3610e53d7a0ee3
Book land <ghaderiheliya91@gmail.com>
2026-07-15 22:57:22 +0330

    Update script.js

--- script.js
+++ script.js
@@ -66,3 +66,19 @@ function topFunction(){
     });
 
 }
+const stars = document.getElementById("stars");
+
+for(let i = 0; i < 100; i++){
+
+    let star = document.createElement("div");
+
+    star.className = "star";
+
+    star.style.left = Math.random() * 100 + "%";
+    star.style.top = Math.random() * 100 + "%";
+
+    star.style.animationDelay = Math.random() * 3 + "s";
+
+    stars.appendChild(star);
+
+}
