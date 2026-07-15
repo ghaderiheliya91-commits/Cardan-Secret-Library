f05197f1baa914b653eb2a436a2110f36b463578
ghaderiheliya91-commits <ghaderiheliya91@gmail.com>
2026-07-15 21:35:03 +0330

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,20 @@ function enterLibrary() {
     }, 800);
 
 }
+const stars = document.getElementById("stars");
+
+for(let i=0;i<120;i++){
+
+    const star=document.createElement("div");
+
+    star.classList.add("star");
+
+    star.style.left=Math.random()*100+"vw";
+
+    star.style.top=Math.random()*100+"vh";
+
+    star.style.animationDelay=Math.random()*2+"s";
+
+    stars.appendChild(star);
+
+}
