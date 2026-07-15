f199b1918ae4e5778d745d16aa959805a4da276c
ghaderiheliya91-commits <ghaderiheliya91@gmail.com>
2026-07-15 21:14:32 +0330

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ document.querySelector("button").addEventListener("click", function(){
     this.style.display = "none";
 
 });
+
+
+function showBook(number){
+
+    let page = document.getElementById("bookPage");
+
+    page.style.display = "block";
+
+    if(number==1){
+
+        page.innerHTML="<h2>The Forgotten Story</h2><p>A forgotten tale hidden in the darkness...</p>";
+
+    }
+
+    if(number==2){
+
+        page.innerHTML="<h2>The Last Page</h2><p>The ending nobody was supposed to read...</p>";
+
+    }
+
+    if(number==3){
+
+        page.innerHTML="<h2>Secrets Between Stars</h2><p>Every star hides a secret...</p>";
+
+    }
+
+}
