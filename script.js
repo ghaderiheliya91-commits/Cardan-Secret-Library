a345ebee989ff691139427279ad68eb3c9489367
Book land <ghaderiheliya91@gmail.com>
2026-07-15 23:07:40 +0330

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ function enterLibrary() {
     setTimeout(function () {
 
         welcome.style.display = "none";
-
         library.style.display = "block";
-
         library.style.opacity = "0";
 
         setTimeout(function () {
@@ -22,57 +20,16 @@ function enterLibrary() {
     }, 800);
 
 }
+
+/* Stars */
+
 const stars = document.getElementById("stars");
 
-for(let i=0;i<120;i++){
+for (let i = 0; i < 120; i++) {
 
-    const star=document.createElement("div");
+    const star = document.createElement("div");
 
     star.classList.add("star");
-
-    star.style.left=Math.random()*100+"vw";
-
-    star.style.top=Math.random()*100+"vh";
-
-    star.style.animationDelay=Math.random()*2+"s";
-
-    stars.appendChild(star);
-
-}
-const topBtn = document.getElementById("topBtn");
-
-window.onscroll = function(){
-
-    if(document.documentElement.scrollTop > 200){
-
-        topBtn.style.display="block";
-
-    }else{
-
-        topBtn.style.display="none";
-
-    }
-
-}
-
-function topFunction(){
-
-    window.scrollTo({
-
-        top:0,
-
-        behavior:"smooth"
-
-    });
-
-}
-const stars = document.getElementById("stars");
-
-for(let i = 0; i < 100; i++){
-
-    let star = document.createElement("div");
-
-    star.className = "star";
 
     star.style.left = Math.random() * 100 + "%";
     star.style.top = Math.random() * 100 + "%";
@@ -80,5 +37,34 @@ for(let i = 0; i < 100; i++){
     star.style.animationDelay = Math.random() * 3 + "s";
 
     stars.appendChild(star);
+
+}
+
+/* Back To Top */
+
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+
+    if (document.documentElement.scrollTop > 200) {
+
+        topBtn.style.display = "block";
+
+    } else {
+
+        topBtn.style.display = "none";
+
+    }
+
+}
+
+function topFunction() {
+
+    window.scrollTo({
+
+        top: 0,
+        behavior: "smooth"
+
+    });
 
 }
